9a03486258b5e90a5cd7bb96d11dc7c856a16b2b
lightCode1840 <1840527533@qq.com>
2023-06-17 17:19:13 +0800

    完成lightCode项目初始化搭建，相关依赖正常引入。

---
+++ light-blog/src/main/java/com/light/LightBlogApplication.java
@@ -0,0 +1,18 @@
+package com.light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lilepingstart
+ * @creat 2023-06-17 16:57
+ */
+
+@SpringBootApplication
+public class LightBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightBlogApplication.class,args);
+    }
+
+}
