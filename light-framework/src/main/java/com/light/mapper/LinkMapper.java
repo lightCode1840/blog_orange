a0f960b2369df40f32f9b9c0836effdc9b79d21e
lightCode1840 <1840527533@qq.com>
2023-07-01 18:11:08 +0800

    新增友链功能

---
+++ light-framework/src/main/java/com/light/mapper/LinkMapper.java
@@ -0,0 +1,16 @@
+package com.light.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.light.domin.entity.Link;
+
+
+/**
+ * 友链(Link)表数据库访问层
+ *
+ * @author makejava
+ * @since 2023-07-01 17:48:01
+ */
+public interface LinkMapper extends BaseMapper<Link> {
+
+}
+
---
+++ light-framework/src/main/java/com/light/vo/ArticleDetailVo.java
@@ -0,0 +1,34 @@
+package com.light.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author lilepingstart
+ * @creat 2023-07-01 17:18
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArticleDetailVo {
+    private Long id;
+    //标题
+    private String title;
+    //文章摘要
+    private String summary;
+
+    private Long categoryId;
+    //所属分类名
+    private String categoryName;
+    //缩略图
+    private String thumbnail;
+    //文章内容
+    private String Content;
+    //访问量
+    private Long viewCount;
+
+    private Date createTime;
+}
